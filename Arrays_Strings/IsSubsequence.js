1562b34d3c23694933f2623b72a5e656b3f34132
jayasudha-1 <138743005+jayasudha-1@users.noreply.github.com>
2025-04-26 23:37:11 -0700

    issubsequence problem

---
+++ Arrays_Strings/IsSubsequence.js
@@ -0,0 +1,12 @@
+var isSubsequence = function(s, t) {
+    let i = 0; // Pointer for string s
+    let j = 0; // Pointer for string t
+    // Iterate through t
+    while (j < t.length) {
+        if (s[i] === t[j]) {
+            i++; // Move to the next character in s if a match is found
+        }
+        j++; // Always move to the next character in t
+    }
+    return i === s.length;
+};
