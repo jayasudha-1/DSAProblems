5fecda7309c5c94e312bc0894ac05bbae83c96bb
jayasudha-1 <138743005+jayasudha-1@users.noreply.github.com>
2025-05-07 21:56:00 -0700

    ransomenote

---
+++ Hashmap_Sets/RansomeNote.js
@@ -0,0 +1,16 @@
+var canConstruct = function (ransomNote, magazine) {
+    const charCount = {};
+
+    for (let char of magazine) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let char of ransomNote) {
+        if (!charCount[char]) {
+            return false;
+        }
+        charCount[char]--;
+    }
+
+    return true;
+};
